dca2ad248b378879965b154f7c9d469b04208ca3
ajkh35 <ajay35@live.in>
2020-03-28 23:26:23 +0530

    First commit

---
+++ app/src/main/java/com/ajay/mediapedia/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ajay.mediapedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
